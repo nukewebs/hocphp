ff1bff4500b29bcef7c232caa59b0570ef3c22dd
Thạo Trịnh <trinhthao@bendoi.vn>
2014-11-06 23:27:27 +0700

    Create luubut.php

---
+++ luubut.php
@@ -0,0 +1,5 @@
+<?php tren(); ?>
+  <h1>Nhận xét của học viên</h1>
+    <p>Cảm ơn hoctudau.com đã mang lại cho tôi sự tự tin về kiến thức và thu nhập cao hơn sau khi tham gia khóa học lập trình web chất lượng với chi phí hợp lý <br><i>Thạo Trịnh</i></p>
+    <img src="img/thaotrinh.jpg" alt="hoctudau" width="200">
+<?php duoi(); ?>
