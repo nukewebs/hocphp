d947d29ad6cdb6c6eaeb8306e9e98c45a8c104ce
Thạo Trịnh <trinhthao@bendoi.vn>
2014-11-06 23:26:59 +0700

    Create lienlac.php

---
+++ lienlac.php
@@ -0,0 +1,9 @@
+<?php tren(); ?>
+ <h1>Góp ý hoặc yêu cầu</h1>
+    <form class="contact" action="">
+	<div> <span>Email</span> <input type="text" placeholder="Email"></div>
+	<div> <span>Ý kiến</span> <textarea name="" id="" cols="30" rows="10" placeholder="Ý kiến"></textarea></div>
+	<input type="submit" value="Gửi">
+	<input type="reset" value="Viết lại">
+	</form>
+<?php duoi(); ?>
