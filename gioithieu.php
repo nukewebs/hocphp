0891c5a9210ca6c6c1f3d3138715f4800106d534
Thạo Trịnh <trinhthao@bendoi.vn>
2014-12-21 15:55:37 +0700

    Delete gioithieu.php

--- gioithieu.php
+++
@@ -1,5 +0,0 @@
-<?php tren(); ?>
-<h1>Đôi nét về chúng tôi</h1>
-   <a href="//Hoctudau.com">Hoctudau.com </a> là trung tâm dạy học trực tuyến có <b> hơn 4 năm kinh </b>nghiệm với <i>hơn 800 hoạc viên</i>, hiện đang đào tạo các môn thiết kế website,<code style="display:inline;padding:0;"> lập trinh website, html, css, javascript, jQuery, PHP, MySQL, Wordpress, Drupal, Magento,... </code>Photoshop, English, Kỹ năng mềm, tin học căn bản (Microsoft Office, Word, Excel, Power Point ...) 
-
-<?php duoi(); ?>
