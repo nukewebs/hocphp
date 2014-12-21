63c44a7b8257db94e4e794fdb3e5b3b131457e45
Thạo Trịnh <trinhthao@bendoi.vn>
2014-12-21 13:34:39 +0700

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include("functions.smart.php"); 
-default_url("trangchu.php"); 
-include($_GET["view"]);
-function	tren() {include("inc_header.php");} 
-function	duoi() {include("inc_footer.php");}
-// lần đầu tiên nghĩ ra và viết include kiểu này do thầy gợi ý tên file có thể sửa lại cho nhanh
-?>
